658146e8e74955487d203f5d05e2f2dce063db50
srikarboga <boggavarapu.srikar@gmail.com>
2024-03-23 12:44:02 -0400

    wrote basic neural network class

--- backend/model.py
+++ backend/model.py
@@ -1,3 +1,21 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms 
 import matplotlib.pyplot as plt
+
+
+class Net(nn.Module, hidden, hidden_size, input_size = 784, output_size = 2):
+    def __init__(self):
+        super().__init__()
+        self.layers = [nn.Linear(input_size, hidden_size)]
+        for _ in range(hidden - 1):
+            self.layers += [nn.Linear(hidden_size, hidden_size)]
+        self.layers.append(nn.Linear(hidden_size, output_size))
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
