e86b92ab63b3f6f51250fe8eaa6098155cbf4a2d
srikarboga <boggavarapu.srikar@gmail.com>
2024-03-23 21:45:36 -0400

    added CORS middleware to setup dev server auto update without running npm run build every single time

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import model
 import uvicorn
 from uuid import uuid4
@@ -7,7 +8,18 @@ from uuid import uuid4
 app = FastAPI()
 
 
-
+origins = [
+    "http://localhost:3000",  # Your React development server
+    # Add other allowed origins if needed
+    #CHANGE THIS WHEN DEPLOYING
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 models = {}
 #api = FastAPI()
@@ -26,7 +38,7 @@ async def new_model():
     return {"user_id": user_id, "model":testlayers}
 
 # Mount the built React app as a static directory
-app.mount("/", StaticFiles(directory="../frontend/build", html=True), name="static")
+#app.mount("/", StaticFiles(directory="../frontend/build", html=True), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- frontend/src/user.js
+++ frontend/src/user.js
@@ -1,6 +1,6 @@
 export async function fetchUserData() {
     try {
-      const response = await fetch("/api/newmodel");
+      const response = await fetch("http://localhost:8000/api/newmodel"); //CHANGE THIS WHEN DEPLOYING: TO /api/newmodel
       const data = await response.json();
       return data.user_id;
     } catch (error) {
